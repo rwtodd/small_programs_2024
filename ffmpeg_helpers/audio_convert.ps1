d82e7ac05744f151f0ac0c458c0f4588b2dccd35
Richard Todd <8326233+rwtodd@users.noreply.github.com>
2024-03-24 12:41:36 -0500

    first shot at python versin of vid->hevc helper script

--- ffmpeg_helpers/audio_convert.ps1
+++ ffmpeg_helpers/audio_convert.ps1
@@ -1,2 +1,2 @@
 # helper powershell script to launch wb-toc on windows
-~\venv\cli\Scripts\python.exe $PSScriptRoot\audio_convert.py $args
+~\venv\cli\Scripts\python.exe $PSScriptRoot\audio_convert $args
---
+++ ffmpeg_helpers/video_convert.ps1
@@ -0,0 +1,2 @@
+# helper powershell script to launch wb-toc on windows
+~\venv\cli\Scripts\python.exe $PSScriptRoot\video_convert $args
